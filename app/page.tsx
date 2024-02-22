793c8a866468ad25f4b7314f25f69b263bb63e9a
Rahul Agarwal <rahulagdev@gmail.com>
2024-02-22 16:40:10 +0530

    Adding toolbar and shapes menu

--- app/page.tsx
+++ app/page.tsx
@@ -4,41 +4,56 @@ import { Room } from "./Room";
 import Navbar from "@/components/Navbar";
 import LeftSideBar from "@/components/LeftSideBar";
 import RightSideBar from "@/components/RightSideBar";
-import {fabric} from 'fabric'
-import { useEffect, useRef } from "react";
-import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { fabric } from "fabric";
+import { useEffect, useRef, useState } from "react";
+import {
+  handleCanvasMouseDown,
+  handleResize,
+  initializeFabric,
+} from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const fabricRef = useRef<fabric.Canvas | null>(null);
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
-  const selectedShapeRef = useRef<string|null>('rectangle');
-  useEffect(()=>{
-    const canvas = initializeFabric({canvasRef, fabricRef});
-    canvas.on("mouse:down", (options)=>{
+  const selectedShapeRef = useRef<string | null>("rectangle");
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: "",
+  });
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+    selectedShapeRef.current = elem?.value as string
+  }
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef });
+    canvas.on("mouse:down", (options) => {
       handleCanvasMouseDown({
-        options, 
+        options,
         canvas,
         isDrawing,
         shapeRef,
-        selectedShapeRef
-      })
-    })
-    window.addEventListener("resize", ()=>{
-      handleResize({fabricRef})
-    })
-  },[])
+        selectedShapeRef,
+      });
+    });
+    window.addEventListener("resize", () => {
+      handleResize({ fabricRef });
+    });
+  }, []);
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar 
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
       <section className="flex h-full flex-row">
         <LeftSideBar />
-        <Live canvasRef={canvasRef}/>
+        <Live canvasRef={canvasRef} />
         <RightSideBar />
-
       </section>
     </main>
   );
 }
-  
