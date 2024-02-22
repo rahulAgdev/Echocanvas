e7c72266f97ff11313cc8489fd8d131226c35924
Rahul Agarwal <rahulagdev@gmail.com>
2024-02-22 15:22:28 +0530

    Adding the left and right sidebars

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,18 @@
 import Live from "@/components/Live";
 import { Room } from "./Room";
 import Navbar from "@/components/Navbar";
+import LeftSideBar from "@/components/LeftSideBar";
+import RightSideBar from "@/components/RightSideBar";
 
 export default function Page() {
   return (
     <main className="h-screen overflow-hidden">
       <Navbar />
       <section className="flex h-full flex-row">
+        <LeftSideBar />
         <Live />
+        <RightSideBar />
+
       </section>
     </main>
   );
